6250c9a0464aebc7388944229727756301a40f98
DiegoCardosoDev <diegocardososcramento@gmail.com>
2022-05-03 23:56:13 -0300

    bytebank-poo-kotlin

--- bytebank/src/main/kotlin/model/Autenticar.kt
+++ bytebank/src/main/kotlin/model/Autenticar.kt
@@ -2,5 +2,6 @@ package model
 
 interface Autenticar {
 
+    val senha: Int
     fun autentica(senha: Int): Boolean
 }
--- bytebank/src/main/kotlin/model/Cliente.kt
+++ bytebank/src/main/kotlin/model/Cliente.kt
@@ -3,7 +3,7 @@ package model
 class Cliente(
      name: String,
      email: String,
-     senha: Int
+     override val senha: Int
 ): Autenticar{
      override fun autentica(senha: Int): Boolean {
           TODO("Not yet implemented")
