20f66948c52b4de06e6465c2e88ef2f524442bd9
DiegoCardosoDev <diegocardososcramento@gmail.com>
2022-05-04 18:56:17 -0300

    override methods

--- bytebank/src/main/kotlin/model/Cliente.kt
+++ bytebank/src/main/kotlin/model/Cliente.kt
@@ -1,10 +1,12 @@
 package model
 
+import model.services.Autenticar
+
 class Cliente(
      name: String,
      email: String,
      override val senha: Int
-): Autenticar{
+): Autenticar {
      override fun autentica(senha: Int): Boolean {
           TODO("Not yet implemented")
      }
